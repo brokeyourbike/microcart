656c13d699d80c167b7a65597ee0f9ee49de6ec4
Ivan Stasiuk <brokeyourbike@gmail.com>
2023-05-09 21:43:35 +0100

    test: config load

--- foundation/config/config_test.go
+++ foundation/config/config_test.go
@@ -1,6 +1,7 @@
 package config_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/brokeyourbike/microcart/foundation/config"
@@ -13,4 +14,19 @@ func TestLoad(t *testing.T) {
 	c, err := config.Load()
 	assert.NoError(t, err)
 	assert.NotNil(t, c)
+}
+
+func TestEnvPrefix(t *testing.T) {
+	os.Setenv("MICROCART_HTTPSERVER_ADDR", "localhost:9000")
+
+	c, err := config.Load()
+	assert.NoError(t, err)
+	assert.Equal(t, "localhost:9000", c.HTTPServer.Addr)
+}
+
+func TestUnparsable(t *testing.T) {
+	os.Setenv("MICROCART_HTTPSERVER_READTIMEOUT", "unparsable")
+
+	_, err := config.Load()
+	assert.Error(t, err)
 }
